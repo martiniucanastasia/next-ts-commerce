dcd89e15ecdeadbf1aa8fbfceae6fa978d9f2ea9
amartiniuc <amartiniuc@griddynamics.com>
2023-03-23 16:12:18 +0200

    Add parent's props for Select Component

--- src/components/UI/Select/types.ts
+++ src/components/UI/Select/types.ts
@@ -3,6 +3,8 @@ export type OptionType<T> = { value: T; label: string };
 export interface SelectProps<T> {
   options: OptionType<T>[];
   placeholder?: string;
+  className?: string;
+  classNamePrefix?: string;
   isSearchable?: boolean;
   isClearable?: boolean;
   onChange: (option: OptionType<T> | null) => void;
