54454c8fc1b987db24684e1b8b167c190b4f9cd0
amartiniuc <amartiniuc@griddynamics.com>
2023-04-03 10:25:04 +0300

    Create one separate section, display it twice

--- src/pages/HomePage/BannerAndProducts/BannerAndProducts.tsx
+++ src/pages/HomePage/BannerAndProducts/BannerAndProducts.tsx
@@ -2,99 +2,77 @@
 
 import { Product } from "@/types/types";
 import { bannerAndProductsStyles as S } from "./styles/bannerAndProductsStyles";
+import { bannerMapper } from "./data";
 
 import Image from "next/image";
 import ArrowRight from "../../../../src/assets/svg/icon/control/arrow_forward.svg";
 
-interface BannerAndProductsProps {
+const BannerAndProductsBlock = ({
+  products,
+  title,
+  image,
+  category,
+}: {
   products: Product[];
-}
+  title: string;
+  image: string;
+  category: string;
+}) => {
+  return (
+    <S.BlockWrapper>
+      <S.BannerAndProductsWrapper>
+        <S.BannerWrapper>
+          <S.InfoWrapper>
+            <S.BannerName>{title}</S.BannerName>
+            <S.BannerButton variant="secondary">Shop now</S.BannerButton>
+          </S.InfoWrapper>
+          <Image src={image} width={280} height={257} alt={"Banner"} />
+        </S.BannerWrapper>
 
-export const BannerAndProducts = ({ products }: BannerAndProductsProps) => {
+        <S.ProductsWrapper>
+          {products
+            .filter((product) => product.category === category)
+            .slice(0, 8)
+            .map((product) => (
+              <S.ProductWrapper key={product.id} href={"/"}>
+                <S.ProductInfo>
+                  <S.ProductName>{product.name}</S.ProductName>
+                  <S.ProductPrice>From USD {product.price}</S.ProductPrice>
+                </S.ProductInfo>
+                <Image
+                  src={product.image}
+                  width={80}
+                  height={80}
+                  alt={product.name}
+                />
+              </S.ProductWrapper>
+            ))}
+        </S.ProductsWrapper>
+      </S.BannerAndProductsWrapper>
+      <S.MobileButton variant="secondary">
+        Shop now
+        <ArrowRight />
+      </S.MobileButton>
+    </S.BlockWrapper>
+  );
+};
+
+export const BannerAndProducts = ({ products }: { products: Product[] }) => {
   return (
     <S.BannerAndProductsContainer>
       <S.BannerAndProductsBody>
-        <S.BlockWrapper>
-          <S.BannerAndProductsWrapper>
-            <S.BannerWrapper>
-              <S.InfoWrapper>
-                <S.BannerName>Home and outdoor</S.BannerName>
-                <S.BannerButton variant="secondary">Shop now</S.BannerButton>
-              </S.InfoWrapper>
-              <Image
-                src={"/home_banner_top.jpg"}
-                width={280}
-                height={257}
-                alt={"Banner top"}
-              />
-            </S.BannerWrapper>
-
-            <S.ProductsWrapper>
-              {products
-                .filter((product) => product.category === "Home")
-                .slice(0, 8)
-                .map((product) => (
-                  <S.ProductWrapper key={product.id} href={"/"}>
-                    <S.ProductInfo>
-                      <S.ProductName>{product.name}</S.ProductName>
-                      <S.ProductPrice>From USD {product.price}</S.ProductPrice>
-                    </S.ProductInfo>
-                    <Image
-                      src={product.image}
-                      width={80}
-                      height={80}
-                      alt={product.name}
-                    />
-                  </S.ProductWrapper>
-                ))}
-            </S.ProductsWrapper>
-          </S.BannerAndProductsWrapper>
-          <S.MobileButton variant="secondary">
-            Shop now
-            <ArrowRight />
-          </S.MobileButton>
-        </S.BlockWrapper>
-
-        <S.BlockWrapper>
-          <S.BannerAndProductsWrapper>
-            <S.BannerWrapper>
-              <Image
-                src={"/home_banner_down.jpg"}
-                width={280}
-                height={257}
-                alt={"Banner down"}
-              />
-              <S.InfoWrapper>
-                <S.BannerName>Consumer electronics and gadgets</S.BannerName>
-                <S.BannerButton variant="secondary">Shop now</S.BannerButton>
-              </S.InfoWrapper>
-            </S.BannerWrapper>
-
-            <S.ProductsWrapper>
-              {products
-                .filter((product) => product.category === "Tech")
-                .slice(0, 8)
-                .map((product) => (
-                  <S.ProductWrapper key={product.id} href={"/"}>
-                    <div>
-                      <S.ProductName>{product.name}</S.ProductName>
-                      <S.ProductPrice>From USD {product.price}</S.ProductPrice>
-                    </div>
-                    <Image
-                      src={product.image}
-                      width={80}
-                      height={80}
-                      alt={product.name}
-                    />
-                  </S.ProductWrapper>
-                ))}
-            </S.ProductsWrapper>
-          </S.BannerAndProductsWrapper>
-          <S.MobileButton variant="secondary">
-            Shop now
-            <ArrowRight />
-          </S.MobileButton>
-        </S.BlockWrapper>
+        <BannerAndProductsBlock
+          products={products}
+          title={bannerMapper.BannerName[0]}
+          image={bannerMapper.BannerImage[0]}
+          category={bannerMapper.ProductCategory[0]}
+        />
+        <BannerAndProductsBlock
+          products={products}
+          title={bannerMapper.BannerName[1]}
+          image={bannerMapper.BannerImage[1]}
+          category={bannerMapper.ProductCategory[1]}
+        />
       </S.BannerAndProductsBody>
     </S.BannerAndProductsContainer>
   );
---
+++ src/pages/HomePage/BannerAndProducts/data.ts
@@ -0,0 +1,5 @@
+export const bannerMapper = {
+  BannerName: ["Home and outdoor", "Consumer electronics and gadgets"],
+  BannerImage: ["/home_banner_top.jpg", "/home_banner_down.jpg"],
+  ProductCategory: ["Home", "Tech"],
+};
