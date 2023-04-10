4763a38bd8c71121dba2187f5cf089cb5edd9e70
amartiniuc <amartiniuc@griddynamics.com>
2023-04-10 17:55:39 +0300

    Add Footer and Subscribe sections to Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,7 @@
 import { NavigationMenu } from "@/components/NavigationMenu/NavigationMenu";
+import { SubscribeSection } from "@/components/Subscribe/SubscribeSection";
+import { Footer } from "@/components/Footer/Footer";
+
 import { getCategories } from "@/helpers/getCategories";
 import { getLanguages } from "../helpers/getLanguage";
 import { getShippingInfo } from "../helpers/getShippingInfo";
@@ -6,7 +9,6 @@ import { getHeaderLinks } from "@/helpers/getHeaderLinks";
 import { getBurgerData } from "@/helpers/getBurgerData";
 
 export default async function Page() {
-  
   const categories = await getCategories();
   const languages = await getLanguages();
   const shippingInfo = await getShippingInfo();
@@ -14,12 +16,16 @@ export default async function Page() {
   const burgerContents = await getBurgerData();
 
   return (
-    <NavigationMenu
-      categories={categories}
-      languages={languages}
-      shippingInfo={shippingInfo}
-      headerLinks={headerLinks}
-      burgerContents={burgerContents}
-    />
+    <>
+      <NavigationMenu
+        categories={categories}
+        languages={languages}
+        shippingInfo={shippingInfo}
+        headerLinks={headerLinks}
+        burgerContents={burgerContents}
+      />
+      <SubscribeSection />
+      <Footer languages={languages} />
+    </>
   );
 }
