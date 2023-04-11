942b36bc60feef0262c50474e48e516f51445d03
amartiniuc <amartiniuc@griddynamics.com>
2023-04-11 15:58:27 +0300

    Add Suppliers section to Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,31 @@
 import { NavigationMenu } from "@/components/NavigationMenu/NavigationMenu";
+import { SuppliersSection } from "@/pages/HomePage/SuppliersSection/SuppliersSection";
+
 import { getCategories } from "@/helpers/getCategories";
 import { getLanguages } from "../helpers/getLanguage";
 import { getShippingInfo } from "../helpers/getShippingInfo";
 import { getHeaderLinks } from "@/helpers/getHeaderLinks";
 import { getBurgerData } from "@/helpers/getBurgerData";
+import { getSuppliers } from "@/helpers/getSuppliers";
 
 export default async function Page() {
-  
   const categories = await getCategories();
   const languages = await getLanguages();
   const shippingInfo = await getShippingInfo();
   const headerLinks = await getHeaderLinks();
   const burgerContents = await getBurgerData();
+  const suppliers = await getSuppliers();
 
   return (
-    <NavigationMenu
-      categories={categories}
-      languages={languages}
-      shippingInfo={shippingInfo}
-      headerLinks={headerLinks}
-      burgerContents={burgerContents}
-    />
+    <>
+      <NavigationMenu
+        categories={categories}
+        languages={languages}
+        shippingInfo={shippingInfo}
+        headerLinks={headerLinks}
+        burgerContents={burgerContents}
+      />
+      <SuppliersSection suppliers={suppliers} />
+    </>
   );
 }
