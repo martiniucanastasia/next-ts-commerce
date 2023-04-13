10a1098f8279b57c11d51f2325b9470c17d3b444
amartiniuc <amartiniuc@griddynamics.com>
2023-04-13 13:17:50 +0300

    Add Banner and Products section to Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,35 @@
 import { NavigationMenu } from "@/components/NavigationMenu/NavigationMenu";
+import { BannerAndProducts } from "@/pages/HomePage/BannerAndProducts/BannerAndProducts";
+
 import { getCategories } from "@/helpers/getCategories";
 import { getLanguages } from "../helpers/getLanguage";
 import { getShippingInfo } from "../helpers/getShippingInfo";
 import { getHeaderLinks } from "@/helpers/getHeaderLinks";
 import { getBurgerData } from "@/helpers/getBurgerData";
+import { getProducts } from "@/helpers/getProducts";
+import { getBannersInfo } from "@/helpers/getBannersInfo";
+
+import { BannersInfo, Product } from "@/types/types";
 
 export default async function Page() {
-  
   const categories = await getCategories();
   const languages = await getLanguages();
   const shippingInfo = await getShippingInfo();
   const headerLinks = await getHeaderLinks();
   const burgerContents = await getBurgerData();
+  const products: Product[] = await getProducts();
+  const bannersInfo: BannersInfo = await getBannersInfo();
 
   return (
-    <NavigationMenu
-      categories={categories}
-      languages={languages}
-      shippingInfo={shippingInfo}
-      headerLinks={headerLinks}
-      burgerContents={burgerContents}
-    />
+    <>
+      <NavigationMenu
+        categories={categories}
+        languages={languages}
+        shippingInfo={shippingInfo}
+        headerLinks={headerLinks}
+        burgerContents={burgerContents}
+      />
+      <BannerAndProducts products={products} bannersInfo={bannersInfo} />
+    </>
   );
 }
